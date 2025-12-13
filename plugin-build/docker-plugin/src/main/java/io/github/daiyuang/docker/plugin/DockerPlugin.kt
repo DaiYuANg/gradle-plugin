d1cc94841d751b1524fa98c1896c1a1e99f91274
DaiYuANg <138393979+DaiYuANg@users.noreply.github.com>
2025-12-13 22:04:48 +0800

    build(docker-plugin): 升级插件版本并增强Docker服务配置
    - 将项目版本从0.0.8更新至0.0.9
    - 在DockerPlugin类中增加对注册表相关参数的支持
    - 添加registryUrl、registryUsername、registryPassword和registryEmail配置项
    - 抑制编译器关于未使用代码的警告
    - 简化DockerService名称引用方式

--- plugin-build/docker-plugin/src/main/java/io/github/daiyuang/docker/plugin/DockerPlugin.kt
+++ plugin-build/docker-plugin/src/main/java/io/github/daiyuang/docker/plugin/DockerPlugin.kt
@@ -4,17 +4,21 @@ import io.github.daiyuang.docker.plugin.service.DockerService
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-@Suppress("UnnecessaryAbstractClass")
+@Suppress("UnnecessaryAbstractClass", "unused")
 abstract class DockerPlugin : Plugin<Project> {
   override fun apply(project: Project) {
     val ext = project.extensions.create(DockerExtension.EXTENSION_NAME, DockerExtension::class.java, project)
     project.gradle.sharedServices.registerIfAbsent(
-      DockerService.Companion.SERVICE_NAME,
+      DockerService.SERVICE_NAME,
       DockerService::class.java,
       { spec ->
         run {
           spec.parameters.host.set(ext.dockerHost)
           spec.parameters.apiVersion.set(ext.apiVersion)
+          spec.parameters.registryUrl.set(ext.registryUrl)
+          spec.parameters.registryUsername.set(ext.registryUsername)
+          spec.parameters.registryPassword.set(ext.registryPassword)
+          spec.parameters.registryEmail.set(ext.registryEmail)
         }
       },
     )
